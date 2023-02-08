46b0c411e7a6702ea26aa29b60e427a33adc6b42
Mihir Bhansali <M_Bhansali@iDev.local>
2023-02-08 14:49:40 -0500

    Understanding Layouts and File Structure in Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { CopyrightComponent } from './copyright/copyright.component';
+import { BodyComponent } from './body/body.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavigationComponent,
+    CopyrightComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule
